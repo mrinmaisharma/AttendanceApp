43d42f535053bd425f1b3c1d6b538c310f0f2944
techNitish <nitishnkkumar.362@gmail.com>
2019-06-24 02:17:11 +0530

    without model

--- bootstrap/cache/9fb785099a3e83ed8fbaffeb3dd28cf796949d43.php
+++ bootstrap/cache/9fb785099a3e83ed8fbaffeb3dd28cf796949d43.php
@@ -5,16 +5,16 @@
 
 <div class="container-fluid">
     <div class="row">
-        <div class="col-md-6 col-lg-3">
+        <div class="col-sm-6 col-lg-3">
             <div class="card">
                 <div class="card-body">
-                    <h3 class="text-black float-right">4565</h3>
+                    <h3 class="text-black float-right style=">45<span style="font-size:0.5em;">Students</span></h3>
                     <h5 class="card-title">Batch A</h5>
-                    
                     <h6 class="card-subtitle mb-2 text-muted">Trainer: Nihal Jaiswal</h6>
-                    
-                    <p class="card-text d-inline"><small class="text-muted">Batch Start 15th August</small>
-                    </p><a href="#" class="card-link float-right"><small>View Details</small></a>
+                    <p class="card-text d-inline"><small class="text-muted">Batch Start 15th August</small></p>
+                    <a href="#" class="card-link float-right"><small>View Details</small></a>
+                    <button type="button" class="btn mb-1 btn-outline-primary"style="margin-top:1em; font-size:0.8em;">Assign Trainer</button>
+                    <button type="button" class="btn mb-1 btn-outline-primary float-right"style="margin-top:1em; font-size:0.8em;">+ Students</button>
                 </div>
             </div>
         </div>
